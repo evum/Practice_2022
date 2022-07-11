b72beb23e2dfcbc3066cde0613cca6365c7a7d4d
evum <evum12@gmail.com>
2022-07-11 18:24:08 +0300

    Final version for 11.07 evening

--- additions/drawing.js
+++ additions/drawing.js
@@ -4,7 +4,7 @@ import count from './count.js';
 const Console = console;
 
 const jsonFile = 'data/cond.json';
-
+let globalNodes;
 const y = {
   min: 0,
   max: 0,
@@ -29,11 +29,20 @@ const elbow = (d) => {
   return '';
 };
 
-function findMax(nodes) {
+function rotate(nodes) {
   const curNodes = nodes;
   const temp = curNodes.x;
   curNodes.x = curNodes.y;
   curNodes.y = temp;
+  if (curNodes.children !== undefined) {
+    curNodes.children.forEach((item) => {
+      rotate(item);
+    });
+  }
+}
+
+function findMax(nodes) {
+  const curNodes = nodes;
   if (curNodes.children === undefined) {
     if (curNodes.x > x.max) {
       x.max = curNodes.x;
@@ -57,31 +66,34 @@ function findMax(nodes) {
   }
 }
 
-function redraw(nodes) {
-}
-
 function click(d) {
   const curD = d;
   Console.log('click ', d);
-  if (d.children) {
-    curD.tempChildren = d.children;
-    curD.children = null;
+  if (curD.children) {
+    curD.tempChildren = curD.children;
+    curD.children = undefined;
   } else {
     curD.children = curD.tempChildren;
-    curD.tempChildren = null;
+    curD.tempChildren = undefined;
   }
-  redraw(d);
+  Console.log('before ', globalNodes);
+  const page = document.getElementById('graph');
+  page.remove();
+  treeBuilding(globalNodes, 0);
 }
 
-function treeBuilding(nodes) {
+function treeBuilding(nodes, invert = 1) {
   const treeNodes = tree(nodes);
-  findMax(nodes);
+  rotate(nodes);
+  if (invert) findMax(nodes);
+  globalNodes = nodes;
   const height = x.max + 100;
   const width = Math.abs(y.min) + y.max + 100;
   svg.attr('width', width);
   svg.attr('height', height);
   // svg.attr('width', findMax(nodes));
   const g = svg.append('g')
+    .attr('id', 'graph')
     .attr('transform', `translate(${Math.abs(y.min) + 60},${30})`);
   const link = g.selectAll('.link');
 
@@ -89,11 +101,14 @@ function treeBuilding(nodes) {
     .enter().append('path')
     .attr('class', 'link')
     .attr('stroke', 'red')
-    .attr('d', elbow);
+    .attr('d', elbow)
+    .exit()
+    .remove();
 
   const node = g.selectAll('.node')
     .data(treeNodes.descendants())
     .enter().append('g')
+    .on('click', click)
     .attr('class', 'node')
     .attr('transform', (d) => `translate(${d.y},${d.x})`);
 
@@ -103,6 +118,7 @@ function treeBuilding(nodes) {
     .attr('fill', (d) => { if (d.data.count === 1) { return 'red'; } return 'white'; })
     .attr('stroke', 'black')
     .attr('stroke-width', 1);
+
   /* Все узлы */
   node.append('foreignObject')
     .attr('class', (d) => {
@@ -120,8 +136,9 @@ function treeBuilding(nodes) {
     .attr('class', (d) => { if (d.data.field) { return 'datText'; } return 'condText'; })
     .append('text')
     .text((d) => { if (d.data.description) { return `\n${d.data.description}`; } return ''; })
-    .attr('class', (d) => { if (d.data.field) { return 'additionText'; } return ''; });
-  node.selectAll('foreignObject').on('click', click);
+    .attr('class', (d) => { if (d.data.field) { return 'additionText'; } return ''; })
+    .exit()
+    .remove();
 }
 
 function draw() {
@@ -131,7 +148,7 @@ function draw() {
     count.counting(json);
     // eslint-disable-next-line no-undef
     const nodes = d3.hierarchy(json, (d) => d.rules);
-    treeBuilding(nodes);
+    treeBuilding(nodes, 1);
   });
 }
 
